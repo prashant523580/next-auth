f41f343278cdd716710de60ae2ce08d3f5ded361
prashant523580 <prashant.thapa62758@gmail.com>
2023-03-26 12:46:13 +0545

    fixed error

--- .history/src/pages/404_20230326124557.tsx
+++ .history/src/pages/404_20230326124557.tsx
@@ -185,7 +185,7 @@ export default function NotFound(){
 					</svg> */}
 
 				</div>
-				<p className="text-sm md:text-base text-yellow-300 p-2 mb-4">The stuff you were looking for doesn&apos;t exist
+				<p className="text-sm md:text-base text-yellow-300 p-2 mb-4">The stuff you were looking for doesn't exist
 				</p>
 				<Link href="/"
 					className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
